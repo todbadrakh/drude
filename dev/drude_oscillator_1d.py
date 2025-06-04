4b62106d3739829cae6f4055dfc81dcf8f2c79e5
Tuguldur T. Odbadrakh <tuguldur.o@gmail.com>
2025-06-03 19:53:38 -0500

    refactored

--- dev/drude_oscillator_1d.py
+++ dev/drude_oscillator_1d.py
@@ -42,3 +42,21 @@ class DrudeOscillator:
     herm = hermval(xi, [0]*state + [1])  # This gives H_state(xi)
     exponential = np.exp(-self.m * self.w * (position - self.position)**2 / 2)
     return norm * coeff * herm * exponential
+  
+  def plot_wavefunction(self, x_range=(-10, 10), num_points=1000, state=None, filename="drude_wavefunction.pdf"):
+        """
+        Plot and save the wavefunction as a PDF.
+        """
+        from matplotlib import pyplot as plt
+        
+        if state is None:
+            state = self.state
+        x = np.linspace(x_range[0], x_range[1], num_points)
+        psi = [self.wavefunction(np.array([xi]), state=state) for xi in x]
+        plt.plot(x, psi)
+        plt.xlabel("Position")
+        plt.ylabel("Wavefunction")
+        plt.title(f"Drude Oscillator Wavefunction (state={state})")
+        plt.grid(True)
+        plt.savefig(filename)
+        plt.close()
--- dev/test.py
+++ dev/test.py
@@ -1,15 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from drude_oscillator import DrudeOscillator
+from drude_oscillator_1d import DrudeOscillator
 
-if __name__ == "__main__":
-    t1 = DrudeOscillator(mass=1.0, charge=1.0, frequency=1.0, position=[0.0], state=3)
-    x = np.linspace(-10, 10, 1000)
-    psi = [t1.wavefunction(np.array([xi])) for xi in x]  # Fixed: use psi, pass xi correctly
-    plt.plot(x, psi)
-    plt.xlabel("Position")
-    plt.ylabel("Wavefunction")
-    plt.title("Drude Oscillator Wavefunction (state=0)")
-    plt.grid(True)
-    plt.savefig("drude_wavefunction.pdf")  # Save as PDF
-    plt.close()
+oscillator = DrudeOscillator(mass=1.0, charge=1.0, frequency=1.0, position=[0.0])
+oscillator.plot_wavefunction(x_range=(-5, 5), num_points=1000, state=1, filename="drude_wavefunction.pdf")
